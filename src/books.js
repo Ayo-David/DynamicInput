04b368552626035391c77e8d59855fbd8b59d1b9
Ayo-David Ajayi <ayodavid.ajayi@gmail.com>
2022-06-23 16:11:17 +0100

    searchengine data

---
+++ src/books.js
@@ -0,0 +1,36 @@
+export const books = [
+    {
+        "author": "Chinua Achebe",
+        "country": "Nigeria",
+        "language": "English",
+        "pages": 209,
+        "title": "Things Fall Apart",
+        "year": 1958
+    },
+    {
+        "author": "Dante Alighieri",
+        "country": "Italy",
+        "language": "Italian",
+        "pages": 928,
+        "title": "The Divine Comedy",
+        "year": 1315
+    },
+    {
+        "author": "Virginia Woolf",
+        "country": "United Kingdom",
+        "language": "English",
+        "pages": 216,
+        "title": "Mrs Dalloway",
+        "year": 1925
+    },
+    {
+        "author": "Virginia Woolf",
+        "country": "United Kingdom",
+        "language": "English",
+        "pages": 209,
+        "title": "To the Lighthouse",
+        "year": 1927
+    }
+
+
+]
